835ef18d77cb01d692d265de28ca3fe370699591
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-11-30 14:48:18 +0000

    minor styles to form and nav bar and fixed bug with bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,7 @@ class BookingsController < ApplicationController
     @timeNow = Time.now
     @startTime = @timeNow.beginning_of_day() + (8*60*60)
     @endTime = @timeNow.beginning_of_day() + (18*60*60)
-    
+
   end
 
   def month
@@ -54,10 +54,10 @@ class BookingsController < ApplicationController
 
     respond_to do |format|
       if @booking.save
-        format.html { redirect_to month_path, notice: 'Booking was successfully created.' }
+        format.html { redirect_to week_path, notice: 'Booking was successfully created.' }
         format.json { render :show, status: :created, location: month_path }
       else
-        format.html { redirect_to month_path, notice: 'Booking overlaps with current booking' }
+        format.html { redirect_to week_path, notice: 'Booking overlaps with current booking' }
         format.json { render json: @booking.errors, status: :unprocessable_entity }
       end
     end
